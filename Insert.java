6dff9a4e4c0330e718b30edf96e6442d3469751f
vanshika705 <vnshkjngd@gmail.com>
2024-07-15 23:21:45 +0530

    third commit

--- Insert.java
+++ Insert.java
@@ -1,21 +1,32 @@
 // to insert element at particular position in array
+
+
+
 public class Insert 
 {
-    public static void main(String[] args) 
+    public static void main(String[] args) // 5 6     2 4 
     {
         int [] arr={0,1,2,3,4};
-        // 5 6     2 4 
-        for(int i=1; i<arr.length; i++)
+        int []position ={2,4} ;
+        int []element ={5,6};
+        int index ;
+        
+        for(int i=0; i<position.length; i++)
         {
-            arr[i] = arr[i-1];
+            index = position[i]-1;           
+            for( i=arr.length-1; i >index ; i--)
+                {
+                    arr[i] = arr[i-1];
+                }
+                arr[index] = element[i];
         }
-        arr[1] = 5; 
-        arr[3] = 6;
-
+        
+        
         for(int i=0; i<arr.length; i++)
         {
             System.out.println(arr[i]);
         }
+        
     }
 }
 
